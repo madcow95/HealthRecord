62dd75ae0348c03b83413117195f8f8ca00efc6e
madcow95 <lobasketve@naver.com>
2023-07-23 21:07:37 +0900

    Feat : 프로젝트 생성
    운동 기록 어플 프로젝트 최초 생성

---
+++ HealthRecord/HealthRecord/HealthRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthRecordApp.swift
+//  HealthRecord
+//
+//  Created by ChoiKwangWoo on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
